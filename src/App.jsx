aadfda724956722eeaacb72cc29967306d964ea7
harshsimform <harsh.v@simformsolutions.com>
2023-05-03 10:19:49 +0530

    createAsyncThunk middleware used to Fetch API

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useDispatch, useSelector } from "react-redux";
+import "./App.css";
+import { fetchTodos } from "./redux/slice/todo";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state);
+  console.log(state);
+  if (state.todo.isLoading) {
+    return <h2>Loading...</h2>;
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={() => dispatch(fetchTodos())}>Fetch Todos</button>
+      {state.todo.data &&
+        state.todo.data.map((obj) => <li key={obj.id}>{obj.title}</li>)}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
